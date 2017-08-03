3ffabfdc868a17f3e671cf43da67e59c68bc42d3
shiri castro <shiri.castro3@gmail.com>
2017-08-03 19:17:27 +0300

    add slide to the picturs

--- javascript.js
+++ javascript.js
@@ -35,8 +35,100 @@ function showContant(name){
 		$('.container').html(data);				
 		if (name === 'home'){
 			homeGallery();
+		}else if(name === 'newCollection' || name === 'argoSuits' || name === 'shoes'){
+			slideImg();
 		}
 
 	});	
 }
+/***add slide***/
+var slideIndex = 1;
+function slideImg(){
+	var imgArray = $('.item img');
+	imgArray.on('click',function(e){
+		gallerySlide(e.target,imgArray);
+	});
+	var imgShoes = $('.shoesContainer img');
+	imgShoes.on('click',function(e){
+		gallerySlide(e.target,imgShoes);
+	});
+}
+
+/***slide popup ***/
+function gallerySlide(img,imgArray){
+	var slideContiner = $('<div>',{
+		id:"popSlide",
+		click:function(e){
+			if(e.target.id === "popSlide"){
+				this.remove();
+			}
+		}
+	}).appendTo('body');
+	var content = $('<div>',{
+		id:"popContainer"
+	}).appendTo(slideContiner);
+	var imgCont = $('<div>',{
+		id:"imgCont"
+	}).appendTo(content);
+
+	imgArray.each(function(i,el){
+		if ($(el).attr('src') === $(img).attr('src')){
+			slideIndex = i + 1;
+		}
+		$('<img>',{
+			class:"imgs",
+			src:$(el).attr('src')
+		}).appendTo(imgCont);
+	});
+
+	$('<button>',{
+		id:"leftButton",
+		text:"<",
+		click:function(e){
+			 e.preventDefault();
+			plusDivs(-1); 
+		}
+	}).appendTo(content);
+	$('<button>',{
+		id:"rightButton",
+		text:">",
+		click:function(e){
+			 e.preventDefault();
+			plusDivs(1); 
+		}
+	}).appendTo(content);
+	$('<button>',{
+	id:"deletButton",
+	text:"X",
+	click:function(e){
+		 e.preventDefault();
+		$('#popSlide').remove(); 
+	}
+	}).appendTo(content);
+
+
+	showDivs(slideIndex);	
+	function plusDivs(n) {
+		slideIndex = slideIndex + n;
+	  	showDivs(slideIndex);
+	}
+
+	function showDivs(n) {
+	  var imgs = $('.imgs');
+	  if (n > imgs.length) {slideIndex = 1}    
+	  if (n < 1) {slideIndex = imgs.length}
+	  	imgs.each(function(i,el){
+	  		$(el).css({display:"none"});
+	  	});
+
+	  $(imgs[slideIndex-1]).css({display:"block"});  
+	}	
+
+}
+$('body').keydown(function (e) {
+    if (e.keyCode === 27) {
+        $('#popSlide').remove();
+    }
+});
+
 
